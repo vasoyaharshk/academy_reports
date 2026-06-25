962ca4be32cd894349a050c1a3c91d407b6cd4a6
vasoyaharshk <vasoyaharshkumar@gmail.com>
2026-06-25 16:18:50 +0200

    ratvillage 3

--- academy_reports/settings.py
+++ academy_reports/settings.py
@@ -1,5 +1,5 @@
-data_directory = '/home/ratvillage02/academy/data/sessions'
-save_directory =  '/home/ratvillage02/plots'
+data_directory = ('/home/ratvillage03/academy/data/sessions')
+save_directory =  '/home/ratvillage03/plots'
 
 
 
